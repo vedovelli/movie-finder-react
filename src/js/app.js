3bd7d381ebc3cf3bd319b92026e73ba5ea26822c
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-16 16:28:13 -0300

    App component.

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 
-console.log('Ready to roll!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1>Movie Finder!</h1>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
