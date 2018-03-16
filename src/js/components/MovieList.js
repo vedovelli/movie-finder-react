127238b14eead22738f312f87956c4a613220111
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-16 18:00:13 -0300

    <MovieList> with better markup

--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -2,9 +2,14 @@
 import React from 'react'
 
 const MovieList = props => {
-  return props.movies.map(({ id, original_title }) => {
-    return <p key={id}>{original_title}</p>
-  })
+  return props.movies.map(({ id, original_title }) => (
+    <li key={id}>
+      <div>
+        <h4>{original_title}</h4>
+        <a href={`movie/${id}`}>Details</a>
+      </div>
+    </li>
+  ))
 }
 
 export default MovieList
