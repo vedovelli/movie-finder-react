7e5a3fb5191fec668c4f1e198ac55b5445199d16
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-16 18:06:09 -0300

    <MovieDetailsContainer> created!

---
+++ src/js/components/MovieDetailsContainer.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react'
+
+class MovieDetailsContainer extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>Movie Details</h1>
+    )
+  }
+}
+
+export default MovieDetailsContainer
