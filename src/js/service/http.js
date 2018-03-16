27384ce1c883191835b3701bcd17b78e54c2f9b0
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-16 17:03:25 -0300

    /service/http.js now has only one responsibility: provide access to the outer world and hold API specific config data

--- src/js/service/http.js
+++ src/js/service/http.js
@@ -1,13 +1,8 @@
 
 import axios from 'axios'
 
-const API_KEY = 'a1279933de606b4374a2c93a1d0127a9'
+export const API_KEY = 'a1279933de606b4374a2c93a1d0127a9'
 
-const http = axios.create({
+export const http = axios.create({
   baseURL: 'https://api.themoviedb.org/3/'
 })
-
-export default {
-  http,
-  key: API_KEY
-}
