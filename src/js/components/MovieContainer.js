01b0541ae51eecfb954829779ee8a20c6de8e03e
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-16 16:35:53 -0300

    <MovieContainer/> created

---
+++ src/js/components/MovieContainer.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+
+class MovieContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    /*
+    * Axios call to the service
+    */
+  }
+  render() {
+    return (
+      <h1>Hello from MovieContainer</h1>
+    )
+  }
+}
+
+export default MovieContainer
