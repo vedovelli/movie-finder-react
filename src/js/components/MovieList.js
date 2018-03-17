07ae05d82f01612245456e0506a4a52c6619f55d
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 16:52:12 -0300

    Now rendering two lists: one based on movies with Poster and another based on movies WITHOUT poster.

--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -4,36 +4,66 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { POSTER_BASE_URL } from '../service/movie-api'
 
+const renderMoviesWithoutPoster = (movies) => (
+  <div className="well">
+    <ul>
+      <h5>Other titles</h5>
+      {movies.map(movie => {
+        return (
+          <li key={movie.id}>
+            <Link to={`movie/${movie.id}`}>{movie.original_title}</Link>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+)
+
+const renderMoviesWithPoster = (movies) => (
+  movies.map(movie => {
+    const styles = {
+      backgroundImage: `url(${POSTER_BASE_URL}${movie.poster_path})`
+    }
+    return (
+      <div className="col-md-4" key={movie.id}>
+        <Link to={`movie/${movie.id}`}>
+          <div className="panel panel-default panel-no-border">
+            <div className="panel-body movie-card" style={styles}></div>
+            <p className="text-center">click to see movie info</p>
+          </div>
+        </Link>
+      </div>
+    )
+  })
+)
+
 class MovieList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      withCover: [],
-      withoutCover: []
+      withPoster: [],
+      withoutPoster: []
     }
   }
 
   //
+  componentDidMount() {
+    const { movies } = this.props
+    this.setState({
+      withPoster: movies.filter(movie => movie.poster_path != null),
+      withoutPoster: movies.filter(movie => movie.poster_path == null)
+    })
+  }
+
+  //
   render() {
+    const { withPoster, withoutPoster } = this.state
     return (
-      <div className="row">
-        {
-          this.props.movies.map(movie => {
-            const styles = {
-              backgroundImage: `url(${POSTER_BASE_URL}${movie.poster_path})`
-            }
-            return (
-              <div className="col-md-4" key={movie.id}>
-                <Link to={`movie/${movie.id}`}>
-                  <div className="panel panel-default panel-no-border">
-                    <div className="panel-body movie-card" style={styles}></div>
-                    <p className="text-center">click to see movie info</p>
-                  </div>
-                </Link>
-              </div>
-            )
-          })
-        }
+      <div>
+        <div className="row">
+          {renderMoviesWithPoster(withPoster)}
+        </div>
+        {withoutPoster.length > 0 && renderMoviesWithoutPoster(withoutPoster)}
       </div>
     )
   }
