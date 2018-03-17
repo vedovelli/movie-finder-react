bd5cd131e4460dd45ee0f03a02024fd9f347dc03
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 08:21:38 -0300

    Fixed an error in the <MovieList>: lack of UL to enclose LIs

--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -3,14 +3,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const MovieList = props => {
-  return props.movies.map(({ id, original_title }) => (
-    <li key={id}>
-      <div>
-        <h4>{original_title}</h4>
-        <a href={`movie/${id}`}>Details</a>
-      </div>
-    </li>
-  ))
+  return (
+    <ul>
+      {
+        props.movies.map(({ id, original_title }) => (
+          <li key={id}>
+            <div>
+              <h4>{original_title}</h4>
+              <a href={`movie/${id}`}>Details</a>
+            </div>
+          </li>
+        ))
+      }
+    </ul>
+  )
 }
 
 MovieList.propTypes = {
