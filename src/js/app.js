428e8d4cae0820551ef5471d9ce8c6a0d2fa9198
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 08:35:56 -0300

    Included router component Switch
    In order to eliminate warning:
    
    browser.js:49 Warning: You tried to redirect to the same route you're
    currently on: "/movies"

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,19 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import App from './components/App'
 import Movies from './components/MoviesContainer'
 
 const markup = (
   <BrowserRouter>
     <App>
-      <Route exact path="/">
-        <Redirect to="/movies" />
-      </Route>
-      <Route exact path="/movies" component={Movies} />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/movies" />
+        </Route>
+        <Route exact path="/movies" component={Movies} />
+      </Switch>
     </App>
   </BrowserRouter>
 )
