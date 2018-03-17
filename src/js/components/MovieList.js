f94c70480a3cb025e75a8a36b1924dd05b177f61
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 16:38:21 -0300

    <MovieLIst> is now a stateful component

--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -1,32 +1,42 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { POSTER_BASE_URL } from '../service/movie-api'
 
-const MovieList = props => {
+class MovieList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      withCover: [],
+      withoutCover: []
+    }
+  }
+
   //
-  return (
-    <div className="row">
-      {
-        props.movies.map(movie => {
-          const styles = {
-            backgroundImage: `url(${POSTER_BASE_URL}${movie.poster_path})`,
-            height: 500
-          }
-          return (
-            <div className="col-md-4" key={movie.id}>
-              <Link to={`movie/${movie.id}`}>
-                <div className="panel panel-default">
-                  <div className="panel-body" style={styles}></div>
-                </div>
-              </Link>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
+  render() {
+    return (
+      <div className="row">
+        {
+          this.props.movies.map(movie => {
+            const styles = {
+              backgroundImage: `url(${POSTER_BASE_URL}${movie.poster_path})`
+            }
+            return (
+              <div className="col-md-4" key={movie.id}>
+                <Link to={`movie/${movie.id}`}>
+                  <div className="panel panel-default panel-no-border">
+                    <div className="panel-body movie-card" style={styles}></div>
+                    <p className="text-center">click to see movie info</p>
+                  </div>
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 MovieList.propTypes = {
