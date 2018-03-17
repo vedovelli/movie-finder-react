3ecde3d642ffd18ad9b727e49e1c3eb0466270a9
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 08:32:59 -0300

    Rect Router configured and the first route (/movies) created

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import App from './components/App'
+import Movies from './components/MoviesContainer'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const markup = (
+  <BrowserRouter>
+    <App>
+      <Route exact path="/">
+        <Redirect to="/movies" />
+      </Route>
+      <Route exact path="/movies" component={Movies} />
+    </App>
+  </BrowserRouter>
+)
+
+ReactDOM.render(markup, document.getElementById('app'))
