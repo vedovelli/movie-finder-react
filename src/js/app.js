56059fb5b15e42378a22de92d53cb8249b66c59b
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-17 08:54:17 -0300

    Route for movie details implemented

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import App from './components/App'
 import Movies from './components/MoviesContainer'
+import MovieDetails from './components/MovieDetailsContainer'
 
 const markup = (
   <BrowserRouter>
@@ -12,7 +13,8 @@ const markup = (
         <Route exact path="/">
           <Redirect to="/movies" />
         </Route>
-        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/movies" component={Movies}/>
+        <Route exact path="/movie/:id" component={MovieDetails} />
       </Switch>
     </App>
   </BrowserRouter>
