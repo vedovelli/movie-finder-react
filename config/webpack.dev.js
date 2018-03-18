b2fb3e3d277b075c1ed17d52e120739ffe86748c
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-18 12:00:17 -0300

    Styles abstracted for its own file. CSS Loader installed for Webpack and properly configured.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -17,6 +17,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types'
+import '../../css/styles.css'
 
 const App = props => {
   return props.children
