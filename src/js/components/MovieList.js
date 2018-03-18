66d05041bdcb1f7d1936aa7e182bdcb6a7620b92
Fabio Vedovelli <vedovelli@gmail.com>
2018-03-18 09:42:59 -0300

    Removed an unused var

--- src/js/components/MovieList.js
+++ src/js/components/MovieList.js
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { POSTER_BASE_URL } from '../service/movie-api'
